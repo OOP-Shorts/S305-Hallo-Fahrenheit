efef4877bff64b82f0988866fc415f367d50decd
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-22 17:21:42 +0100

    Add solution

--- src/HalloFarenheit.java
+++ src/HalloFarenheit.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class HalloFarenheit {
 
-    public static void main(String[] args) {
+    public static float readTemperatureInCelsius() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Bitte gib einen Temperaturwert in Celsius ein: ");
+        return console.nextFloat();
+    }
 
+    public static void main(String[] args) {
+        float temperatureInCelsius = readTemperatureInCelsius();
+        float temperatureInFahrenheit = (temperatureInCelsius * 1.8f) + 32;
+        System.out.println(temperatureInCelsius + "°C entsprechen " + temperatureInFahrenheit + "°F");
     }
 }
